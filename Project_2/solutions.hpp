fdeba0368784570a2ab5a41090a4e650da4728a4
Grant <grantjtatge@gmail.com>
2025-09-26 16:12:49 -0700

    Init project2 files

---
+++ Project_2/solutions.hpp
@@ -0,0 +1,12 @@
+#ifndef __MY_SOLUTION_HPP__
+#define __MY_SOLUTION_HPP__
+
+#include<iostream>
+#include"reclists.hpp"
+
+int numNodesAtTheTopLevel(list p);
+list append(list p, list q);
+list reverseTopLevel(list p);
+
+
+#endif
